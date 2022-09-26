bad88ea9b628463b6f69f256fe08e429c112d5ce
nithin22796 <nithin22796@github.com>
2022-09-26 15:40:09 +0530

    Sending parallel request

--- src/API/factsAPI.js
+++ src/API/factsAPI.js
@@ -1,12 +1,26 @@
-
 const baseURL = "https://catfact.ninja";
-
 
 export const getFacts = async (limit = 10) => {
   let FACTS = [];
+  let apis = [];
   for (let i = 1; i <= limit; i++) {
-    await fetch(`${baseURL}/fact`).then(res => res.json()).then((data) => FACTS.push(data.fact));
+    apis.push(`${baseURL}/fact`);
   }
-  console.log(FACTS);
+  try {
+    const requests = apis.map((url) => fetch(url));
+    const responses = await Promise.all(requests);
+    const errors = responses.filter((response) => {
+      console.log(response);
+      return !response.ok;
+    });
+    if (errors.length > 0) {
+      throw errors.map((response) => Error(response.statusText));
+    }
+    const json = responses.map((response) => response.json());
+    const data = await Promise.all(json);
+    data.forEach((datum) => FACTS.push(datum.fact));
+  } catch (errors) {
+    errors.forEach((error) => console.error(error));
+  }
   return FACTS;
-}
+};
--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import {getFacts} from "./API/factsAPI";
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { getFacts } from "./API/factsAPI";
+import { useEffect, useState } from "react";
 
 function App() {
   const [facts, setFacts] = useState(null);
   const fetchFacts = async () => {
     setFacts(null);
-    let allFacts =  await getFacts();
+    let allFacts = await getFacts();
     setFacts(allFacts);
-  }
+  };
   useEffect(() => {
     fetchFacts();
     console.log(facts);
@@ -19,11 +19,17 @@ function App() {
       {facts && facts.length > 0 ? (
         <>
           <button onClick={fetchFacts}>Refresh Facts</button>
-          <div className='mainFrame'>
-            {facts.map((fact, index) => <div className='list-item' key={index}>{fact}</div>)}
+          <div className="mainFrame">
+            {facts.map((fact, index) => (
+              <div className="list-item" key={index}>
+                {fact}
+              </div>
+            ))}
           </div>
         </>
-      ) : <div className='loader'></div>}
+      ) : (
+        <div className="loader"></div>
+      )}
     </div>
   );
 }
